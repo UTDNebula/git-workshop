a38b1d3de74be3bc458e1edfc6bda2bd2b87e37d
Bryant <bryantchen1807@gmail.com>
2026-08-31 20:17:34 -0500

    new banner

--- calculator.c
+++ calculator.c
@@ -25,7 +25,7 @@ int multiply(int a, int b)
 }
 
 int main(void) {
-    print_banner("Nebula Labs Github Workshop\n");
+    print_banner("Nebula Labs Git/Github Workshop\n");
 
     int num1 = 12;
     int num2 = 4;
